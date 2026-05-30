9f5c17c6e5176e49e49a0066ac30efb267b1a248
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2026-05-30 17:08:14 +0200

    activated service worker in manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
+import webExtension from "vite-plugin-web-extension"
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    webExtension({
+      manifest: "public/manifest.json",
+      disableAutoLaunch: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
