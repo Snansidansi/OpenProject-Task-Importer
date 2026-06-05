173395c8547cc612a7ea003e25a041236bab1048
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2026-06-05 20:22:53 +0200

    Updated system prompt to denie using field with unknown types and also send name field of types (for custom fields)

--- src/llmRequest.ts
+++ src/llmRequest.ts
@@ -107,7 +107,6 @@ export class LlmRequest {
           }
 
           delete (field as any).allowedForLLM
-          delete (field as any).name
           delete (field as any).location
 
           if (field.values && Array.isArray(field.values)) {
@@ -133,9 +132,11 @@ export const defaultSystemPrompt =
   "Its your job to choose a task type." +
   "In the task schemas include a type for each filed which you need to look up (if its not a basic type like string or boolean) in the types section of this prompt for more information." +
   "If a not basic type is not specified ignore that field." +
+  "Even ignore the field if the user explicitly asks for it and you do not know the type so NEVER fill out a field without knowing how the type works." +
   "You need to fill out every field that has true for the required field and for the not required fields its your choice if you fill them out." +
-  "There also can be description for a field in the schema that you should respect when filling out that field in your response" +
-  '(E.g. {"taskname": {"type": "string", "description\:"max 255 chars"}} means that there is a field named taskname which is a string with a maximum length of 255 characters).' +
+  'There also can be description for a field in the schema that you should respect when filling out that field in your response and a name field so you know what the field means (important for fields like "customType12") ' +
+  '(E.g. {"taskname": {"type": "string", "description\:"max 255 chars", "name": "Work package name"}} means that there is a field represents the work package name and is named "taskname" which is a string with a maximum length of 255 characters).' +
+  'So the name field is just for your information dont write e.g. "Work package namw": {...} in your response but "taskname": {...} instead.' +
   "Some descriptions and other text may be in a different language then english which you should adopt to." +
   "You absolutely need to responde in the following json format otherwise you failed your job:" +
   "Answer in one json object which is a list of all the task objects." +
